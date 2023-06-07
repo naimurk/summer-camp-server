62da12f161f429bd77df2c007e0538f1968bf116
naimurk <naimurk781@gmail.com>
2023-06-07 14:10:10 +0600

    home page banner,class,instructor section done

--- index.js
+++ index.js
@@ -28,7 +28,31 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
+
     await client.connect();
+  
+    const classCollection = client.db('summer').collection('classes');
+    const instructorCollection = client.db('summer').collection('instructorCollection');
+// classes
+    app.get('/classes', async(req, res)=> {
+      try {
+        const result = await classCollection.find().toArray()
+        res.send(result)
+      } catch (error) {
+        res.send(error)
+      }
+    })
+
+  // instructor
+    app.get('/instructors', async(req, res)=> {
+      try {
+        const result = await instructorCollection.find().toArray()
+        res.send(result)
+      } catch (error) {
+        res.send(error)
+      }
+    })
+     
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
