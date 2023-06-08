a4f438fc6b2ff7ea040e3a2fdee740c3a9b3a31d
naimurk <naimurk781@gmail.com>
2023-06-08 19:19:02 +0600

    stripe install and its api done

--- index.js
+++ index.js
@@ -3,200 +3,244 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 const jwt = require('jsonwebtoken');
-require('dotenv').config()
+const stripe = require("stripe")('sk_test_51NFV1sCJxnWqbAIJfVOOrL4GqlS9x7P1Wk45p0GZu3Tuokk1GtY9Y5SMukFBJrVbRmnMxhfFAfjfbSNy0mM1Gqmf00UkaWgpKH')
 
-// midleWare
-app.use(express.json())
-app.use(cors())
+require('dotenv').config();
 
+// Middleware
+app.use(express.json());
+app.use(cors());
 
-// verify token midleware
+// Verify token middleware
 const verifyJwt = (req, res, next) => {
   const authorization = req.headers?.authorization;
-  // console.log('authorization', authorization);
   if (!authorization) {
-    res.status(404).send({ error: true, message: 'no token' })
+    return res.status(404).send({ error: true, message: 'No token' });
   }
-  else {
-    const token = authorization.split(' ')[1];
-    // console.log( 'token',token);
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-      if (err) {
-        res.status(403).send({ error: true, message: 'unauthorized access' })
-      }
-      else {
-        req.decoded = decoded
-        // console.log(req.decoded.email);
-        next()
-      }
-    })
-  }
-}
 
+  const token = authorization.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(403).send({ error: true, message: 'Unauthorized access' });
+    }
 
+    req.decoded = decoded;
+    next();
+  });
+};
 
-
-// connect mongodb
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
+// Connect to MongoDB
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.50l1tkw.mongodb.net/?retryWrites=true&w=majority`;
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-
     await client.connect();
 
     const classCollection = client.db('summer').collection('classes');
     const instructorCollection = client.db('summer').collection('instructorCollection');
-    const userCollection = client.db('summer').collection('users')
+    const userCollection = client.db('summer').collection('users');
+    const cartCollection = client.db('summer').collection('carts');
 
-
-    // json web token create api
+    // JSON Web Token create API
     app.post('/jwt', (req, res) => {
       try {
         const user = req.body;
-        // console.log(user);
         const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
-        res.send({ token })
+        res.send({ token });
       } catch (error) {
-        res.send(error)
+        res.status(500).send({ error: true, message: 'Internal server error' });
       }
-    })
+    });
 
-
-
-    // classes
+    // Classes API
     app.get('/classes', async (req, res) => {
       try {
-        const result = await classCollection.find().toArray()
-        res.send(result)
+        const result = await classCollection.find().toArray();
+        res.send(result);
       } catch (error) {
-        res.send(error)
+        res.status(500).send({ error: true, message: 'Internal server error' });
       }
-    })
+    });
 
-    // instructor
+    // Instructors API
     app.get('/instructors', async (req, res) => {
       try {
-        const result = await instructorCollection.find().toArray()
-        res.send(result)
+        const result = await instructorCollection.find().toArray();
+        res.send(result);
       } catch (error) {
-        res.send(error)
+        res.status(500).send({ error: true, message: 'Internal server error' });
       }
-    })
+    });
 
-    // user collection 
+    // User registration API
     app.post('/users', async (req, res) => {
       try {
         const user = req.body;
+        const query = { email: user.email };
+        const existingUser = await userCollection.findOne(query);
 
-        // console.log(user.email);
-        const query = { email: user.email }
-        const existingUser = await userCollection.findOne(query)
-        // console.log('existing user', existingUser);
         if (existingUser) {
-          res.send({ message: 'user already exist' })
-        }
-        else {
-          const result = await userCollection.insertOne(user);
-          res.send(result)
+          return res.send({ message: 'User already exists' });
         }
 
+        const result = await userCollection.insertOne(user);
+        res.send(result);
       } catch (error) {
-        res.send(error)
+        res.status(500).send({ error: true, message: 'Internal server error' });
       }
-    })
+    });
 
-    // all user 
-    app.get('/users', async (req, res) => {
+    // All users API
+    app.get('/all-users', async (req, res) => {
+      try {
+        const result = await userCollection.find().toArray();
+        res.send(result);
+      } catch (error) {
+        res.status(500).send({ error: true, message: 'Internal server error' });
+      }
+    });
 
-      const result = await userCollection.find().toArray()
-      res.send(result)
-
-    })
-
-
-
-    // (_________ is Admin apis_______)
+    // Check if user is admin API
     app.get('/users/admin/:email', verifyJwt, async (req, res) => {
       try {
-
         const email = req.params.email;
         if (req.decoded.email !== email) {
-          res.send({ admin: false });
+          return res.send({ admin: false });
         }
+
         const query = { email: email };
         const user = await userCollection.findOne(query);
         const result = { admin: user?.role === 'admin' };
-        // console.log(result);
-        res.send(result)
-
+        res.send(result);
       } catch (error) {
-        res.send(error.name, error.message)
+        res.status(500).send({ error: true, message: 'Internal server error' });
       }
-    })
-    // (_________ is instructor apis_______)
+    });
+
+    // Check if user is instructor API
     app.get('/users/instructor/:email', verifyJwt, async (req, res) => {
       try {
-
         const email = req.params.email;
         if (req.decoded.email !== email) {
-          res.send({ admin: false });
+          return res.send({ admin: false });
         }
+
         const query = { email: email };
         const user = await userCollection.findOne(query);
         const result = { admin: user?.role === 'instructor' };
-        // console.log(result);
-        res.send(result)
-
+        res.send(result);
       } catch (error) {
-        res.send(error.name, error.message)
+        res.status(500).send({ error: true, message: 'Internal server error' });
       }
-    })
-    // (_________ is student apis_______)
+    });
+
+    // Check if user is student API
     app.get('/users/student/:email', verifyJwt, async (req, res) => {
       try {
-
         const email = req.params.email;
         if (req.decoded.email !== email) {
-          res.send({ admin: false });
+          return res.send({ admin: false });
         }
+
         const query = { email: email };
         const user = await userCollection.findOne(query);
         const result = { admin: user?.role === 'student' };
-        // console.log(result);
-        res.send(result)
-
+        res.send(result);
       } catch (error) {
-        res.send(error.name, error.message)
+        res.status(500).send({ error: true, message: 'Internal server error' });
       }
-    })
+    });
 
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // Cart creation API
+    app.post('/carts', async (req, res) => {
+      try {
+        const item = req.body;
+        const result = await cartCollection.insertOne(item);
+        res.send(result);
+      } catch (error) {
+        res.status(500).send({ error: true, message: 'Internal server error' });
+      }
+    });
+
+    // Get user's carts API
+    app.get('/my-carts', verifyJwt, async (req, res) => {
+      try {
+        const email = req.query.email;
+        if (!email) {
+          return res.send([]);
+        }
+
+        const decodedEmail = req.decoded.email;
+        if (email !== decodedEmail) {
+          return res.status(403).send({ error: true, message: 'Forbidden access' });
+        }
+
+        const query = { email: email };
+        const result = await cartCollection.find(query).toArray();
+        res.send(result);
+      } catch (error) {
+        res.status(500).send({ error: true, message: 'Internal server error' });
+      }
+    });
+
+  //  Cart delete api 
+  app.delete('/carts/:id', async (req, res) => {
+    const id = req.params.id
+    const query = { _id: new ObjectId(id) }
+    const result = await cartCollection.deleteOne(query)
+    res.send(result)
+  })
+
+
+  // stripe  api
+  app.post('/create-payment-intent', verifyJwt, async (req, res) => {
+    try {
+      const { price } = req.body;
+  
+      if (!price) {
+        return res.status(400).send('Price is missing');
+      }
+  
+      const amount = Math.round(100 * price); // Convert price to cents
+  
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount,
+        currency: 'usd',
+        payment_method_types: ['card']
+      });
+  
+      res.send({ clientSecret: paymentIntent.client_secret });
+    } catch (error) {
+      console.error(error);
+      res.status(500).send({ error: 'An error occurred' });
+    }
+  });
+   
+
+
+
+
+    // Default route
+    app.get('/', (req, res) => {
+      res.send('Summer is running');
+    });
+
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    // Ensures that the client will close when you finish/error
+    // Close the MongoDB connection when finished
     // await client.close();
   }
 }
-run().catch(console.dir);
 
-
-app.get('/', (req, res) => {
-  res.send('summer is running ')
-})
-
-app.listen(port, () => {
-  console.log(port, "port is okay");
-})
+run().catch(console.error);
