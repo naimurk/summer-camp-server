ebe135e87df59aa05751e991ae539fa0d231904b
naimurk <naimurk781@gmail.com>
2023-06-11 20:00:01 +0600

    server side deploy

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 const jwt = require('jsonwebtoken');
-const stripe = require("stripe")(process.env.STRIPE_SECRET)
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY)
 
 require('dotenv').config();
 
